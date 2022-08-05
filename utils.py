c39b25308eba29060fd51063279bc2822541f797
Alan <leggitta3@gmail.com>
2022-08-05 12:21:27 -0400

    creating dev branch to store exploratory notebooks

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import torchvision.transforms as T
 from torchvision.utils import make_grid
 
 
-img_path = '/home/alan/Projects/gen_dnd_art/posed_images/im128/*pkl'
+img_path = '/home/alan/Projects/gen_dnd_art/filtered_images/im128/*pkl'
 img_files = glob.glob(img_path)
 
 # determine class names
@@ -22,10 +22,11 @@ class ImSet(Dataset):
         super().__init__()
         self.img_files = glob.glob(img_path)
         self.transform = T.Compose([
-            # add random noise
-            lambda x: np.clip(x + np.random.randn(*x.shape) / 5, 0, 1),
-            # convert to tensor
             T.ToTensor(),
+            T.ColorJitter(0.1, 0.1, 0.1, 0.1),
+            T.RandomHorizontalFlip(),
+            # add random noise and clip
+            lambda x: torch.clip(torch.randn(x.shape) / 20 + x, 0, 1),
             T.Normalize(0.5, 0.5)
         ])
     
@@ -52,36 +53,56 @@ class ImSet(Dataset):
         return (img_fname, img, species_, class_, gender_)
 
 class VariationalEncoder(nn.Module):
-    def __init__(self, input_channels=3):
+    def __init__(self, input_channels=3, latent_size=4096):
         super().__init__()
         
+        self.latent_size = latent_size
+        
         self.net = nn.Sequential(
-            nn.Conv2d(input_channels, 8, 4, 2),
+            # 128 -> 63
+            nn.Conv2d(input_channels, 16, 4, 2),
             nn.LeakyReLU(0.2),
             
-            nn.Conv2d(8, 16, 3, 2),
-            nn.LeakyReLU(0.2),
-            
+            # 63 -> 31
             nn.Conv2d(16, 32, 3, 2),
             nn.LeakyReLU(0.2),
-
+            
+            # 31 -> 15
             nn.Conv2d(32, 64, 3, 2),
             nn.LeakyReLU(0.2),
 
+            # 15 -> 7
             nn.Conv2d(64, 128, 3, 2),
             nn.LeakyReLU(0.2),
 
+            # 7 -> 5
             nn.Conv2d(128, 256, 3, 1),
+            nn.LeakyReLU(0.2),
+
+            # 5 -> 4
+            nn.Conv2d(256, 512, 2, 1),
+            nn.LeakyReLU(0.2),
+            
+            # 4 -> 3
+            nn.Conv2d(512, 1024, 2, 1),
+            nn.LeakyReLU(0.2),
+            
+            # 3 -> 2
+            nn.Conv2d(1024, 2048, 2, 1),
+            nn.LeakyReLU(0.2),
+            
+            # 2 -> 1
+            nn.Conv2d(2048, latent_size, 2, 1),
             nn.LeakyReLU(0.2),
             
             nn.Flatten(),
-            nn.Linear(256, 256),
-            nn.Dropout(0.4)
+            nn.Linear(latent_size, latent_size),
+            # nn.Dropout(0.4)
         )
         
         # parameters for variational autoencoder
-        self.mu = nn.Linear(256, 256)
-        self.sigma = nn.Linear(256, 256)
+        self.mu = nn.Linear(latent_size, latent_size)
+        self.sigma = nn.Linear(latent_size, latent_size)
         self.N = torch.distributions.Normal(0, 1)
         self.N.loc = self.N.loc.cuda()
         self.N.scale = self.N.scale.cuda()
@@ -97,8 +118,8 @@ class VariationalEncoder(nn.Module):
         return x
 
 class ConditionalEncoder(VariationalEncoder):
-    def __init__(self):
-        super().__init__(input_channels=4)
+    def __init__(self, latent_size=4096):
+        super().__init__(input_channels=4, latent_size=latent_size)
         
         self.emb_species = nn.Embedding(len(species), 128**2 // 3 + 128**2 % 3)
         self.emb_class = nn.Embedding(len(classes), 128**2 // 3)
@@ -125,32 +146,50 @@ class ConditionalEncoder(VariationalEncoder):
 
     
 class Decoder(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_size=4096):
         super().__init__()
-        
+        self.latent_size = latent_size
         self.net = nn.Sequential(
             
-            nn.Linear(256, 256),
-            nn.Dropout(0.4),
+            nn.Linear(latent_size, latent_size),
+            # nn.Dropout(0.4),
             
-            nn.Unflatten(1, (256, 1, 1)),
+            nn.Unflatten(1, (latent_size, 1, 1)),
             
+            # 1 -> 2
+            nn.ConvTranspose2d(latent_size, 2048, 2, 1),
+            nn.LeakyReLU(0.2),
+            
+            # 2 -> 3
+            nn.ConvTranspose2d(2048, 1024, 2, 1),
+            nn.LeakyReLU(0.2),
+            
+            # 3 -> 4
+            nn.ConvTranspose2d(1024, 512, 2, 1),
+            nn.LeakyReLU(0.2),
+            
+            # 4 -> 5
+            nn.ConvTranspose2d(512, 256, 2, 1),
+            nn.LeakyReLU(0.2),
+            
+            # 5 -> 7
             nn.ConvTranspose2d(256, 128, 3, 1),
             nn.LeakyReLU(0.2),
             
+            # 7 -> 15
             nn.ConvTranspose2d(128, 64, 3, 2),
             nn.LeakyReLU(0.2),
             
+            # 15 -> 31
             nn.ConvTranspose2d(64, 32, 3, 2),
             nn.LeakyReLU(0.2),
             
+            # 31 -> 63
             nn.ConvTranspose2d(32, 16, 3, 2),
             nn.LeakyReLU(0.2),
-            
-            nn.ConvTranspose2d(16, 8, 3, 2),
-            nn.LeakyReLU(0.2),
 
-            nn.ConvTranspose2d(8, 3, 4, 2),
+            # 63 -> 128
+            nn.ConvTranspose2d(16, 3, 4, 2),
             nn.Tanh()
         )
     
@@ -159,13 +198,13 @@ class Decoder(nn.Module):
 
 
 class ConditionalDecoder(Decoder):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, latent_size=4096):
+        super().__init__(latent_size)
         
-        self.emb_species = nn.Embedding(len(species), 256 // 3 + 256 % 3)
-        self.emb_class = nn.Embedding(len(classes), 256 // 3)
-        self.emb_gender = nn.Embedding(len(genders), 256 // 3)
-        self.label_net = nn.Linear(512, 256)
+        self.emb_species = nn.Embedding(len(species), latent_size // 3 + latent_size % 3)
+        self.emb_class = nn.Embedding(len(classes), latent_size // 3)
+        self.emb_gender = nn.Embedding(len(genders), latent_size // 3)
+        self.label_net = nn.Linear(2*latent_size, latent_size)
         
     def forward(self, Z, species_, class_, gender_):
         x = self.emb_species(species_)
@@ -179,26 +218,48 @@ class ConditionalDecoder(Decoder):
 
     
 class VariationalAutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_size=4096):
         super().__init__()
-        self.enc = VariationalEncoder()
-        self.dec = Decoder()
+        self.latent_size = latent_size
+        self.enc = VariationalEncoder(latent_size)
+        self.dec = Decoder(latent_size)
     
     def forward(self, x):
         return self.dec(self.enc(x))
 
 class ConditionalVariationalAutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_size=4096):
         super().__init__()
-        self.enc = ConditionalEncoder()
-        self.dec = ConditionalDecoder()
+        self.latent_size = latent_size
+        self.enc = ConditionalEncoder(latent_size)
+        self.dec = ConditionalDecoder(latent_size)
     
     def forward(self, img, species_, class_, gender_):
         Z = self.enc(img, species_, class_, gender_)
         x = self.dec(Z, species_, class_, gender_)
         return x
 
-def show_tensor(Z, ax):
+class ConditionalDiscriminator(ConditionalEncoder):
+    def __init__(self, latent_size=4096):
+        super().__init__(latent_size)
+        self.dsc = nn.Sequential(
+            nn.Linear(latent_size, 1),
+            nn.Sigmoid()
+        )
+    def forward(self, img, species_, class_, gender_):
+        x = self.emb_species(species_)
+        y = self.emb_class(class_)
+        z = self.emb_gender(gender_)
+        
+        x = torch.concat([x, y, z], dim=1)
+        x = self.emb_reshape(x)
+        
+        x = torch.concat([img, x], dim=1)
+        x = self.net(x)
+        x = self.dsc(x)
+        return x
+    
+def show_tensor(Z, ax, **kwargs):
     if len(Z.shape) > 3:
         Z = Z[0]
     
@@ -206,5 +267,5 @@ def show_tensor(Z, ax):
         Z = (Z + 1) / 2
     
     Z = np.transpose(Z.detach().cpu().numpy(), (1, 2, 0))
-    ax.imshow(Z)
+    ax.imshow(Z, **kwargs)
     return ax
